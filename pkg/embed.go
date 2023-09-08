f3ffdc705b728e33525d0849eeddcc33ee23774b
Gaiaz Iusipov <g.iusipov@gmail.com>
2023-09-08 09:15:35 +0800

    Fix typo

--- pkg/embed.go
+++ pkg/embed.go
@@ -1,5 +1,5 @@
 // Package swaggerui contains static SwaggerUI files filesystem.
-// To fill this package with a files run `make swaggerui`
+// To fill this package with a files run `make swagger-ui`
 package swaggerui
 
 import "embed"
